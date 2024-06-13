388c6d6bcc6fb4dfcd336b7367911ed36d7df489
Jacob Groner <jacobgroner@gmail.com>
2024-06-13 13:49:11 -0400

    removed tutorials and resources

--- tutorials/timer.py
+++
@@ -1,8 +0,0 @@
-# First, import the timer class.
-from high_precision_timer.precision_timer import PrecisionTimer
-import time as tm
-
-timer = PrecisionTimer("s")
-timer.reset()
-tm.sleep(3)
-print(timer.elapsed)
