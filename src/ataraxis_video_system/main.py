b095c9ac2a9100ffd307695643ea0ebc033e49e6
Ivan Kondratyev <140765606+Inkaros@users.noreply.github.com>
2024-06-13 16:30:53 -0400

    Adjusted main.py to work given the new name of the video_system script

--- src/ataraxis_video_system/main.py
+++ src/ataraxis_video_system/main.py
@@ -1,18 +1,18 @@
-# First, import the Videosystem and Camera classes
-from video_system.ataraxis_video_system import Videosystem, Camera
+# First, import the VideoSystem and Camera classes
+from ataraxis_video_system.video_system import VideoSystem, Camera
 import time
 
 # Since the video system uses multiprocessing, it should always be called within '__main__' scope
 if __name__ == "__main__":
-    vs = Videosystem("imgs", Camera()) # Create the system using the built in camera class
-    vs.start() # Start the system, activates camera and begins takign and saving images
+    vs = VideoSystem("imgs", Camera())  # Create the system using the built-in camera class
+    vs.start()  # Start the system, activates camera and begins taking and saving images
     time.sleep(5)
-    vs.stop_image_collection() # Stop the camera from taking any more pictures but continue image saving
+    vs.stop_image_collection()  # Stop the camera from taking any more pictures but continue image saving
     time.sleep(5)
-    vs.stop() # End the system, discarding any unsaved images
+    vs.stop()  # End the system, discarding any unsaved images
 
 
 # if __name__ == "__main__":
-#     vs = Videosystem("imgs", Camera())
+#     vs = VideoSystem("imgs", Camera())
 #     vs.start(True)
 #     input()  # Need this line because subprocesses are daemon, as soon as you leave the __main__ scope the processes stop
