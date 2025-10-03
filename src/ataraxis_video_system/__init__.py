3f5fb8a38c6d93f4b0278e4ce32f8c689f955041
Inkaros <ikondratv@gmail.com>
2025-10-03 14:34:57 -0400

    -- Added check_gpu_availability() and check_ffmpeg_availability() standalone functions to the saver.py module. Previously, the logic for these functions was defined separately in the VideoSystem's 'add_saver()' method. -- Moved 'get_opencv_ids' and 'get_harvesters_ids' VideoSystem methods to camera.py module as standalone functions. -- Deprecated 'add_saver' and 'add_camera' methods of the VideoSystem, class. Now all initialization logic and checks are performed as part of the main VideoSystem __init__ method. -- OpenCVCamera now checks and, if necessary, raises errors if it is unable to apply the user-defined aprameters to the managed camera hardware. -- Deprecated the helper _CameraSystem class. -- Renamed CameraBackends enumeration to CameraInterfaces. -- Streamlined the VideoSystem __init__ logic and arguments by removing unnecessary arguments and optimizing the error-checking and initialization logic where possible. -- Deprecated BGRA color format support. -- Optimized the VideoSystem __init__ and class documentation. -- camera id checking functions now package the ID data for each discovered camera into a CameraInformation dataclass instance. -- Refactored library description to reflect the deprecation of the ImageSaver and all associated functionality

--- src/ataraxis_video_system/__init__.py
+++ src/ataraxis_video_system/__init__.py
@@ -1,26 +1,44 @@
-"""A Python library that interfaces with a wide range of cameras to flexibly record visual stream data as images or
-videos.
+"""A Python library that interfaces with a wide range of cameras to flexibly record visual stream data as videos.
 
 See https://github.com/Sun-Lab-NBB/ataraxis-video-system for more details.
 API documentation: https://ataraxis-video-system-api-docs.netlify.app/
 Authors: Ivan Kondratyev (Inkaros), Jacob Groner, Natalie Yeung
 """
 
+import os
+import multiprocessing as mp
+
+# Applies important library-wide configurations to optimize runtime performance.
+if mp.get_start_method(allow_none=True) is None:
+    # Makes the library behave the same way across all platforms.
+    mp.set_start_method("spawn")
+
+# Improves frame rendering (display) on Windows operating systems.
+os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
+
+
 from .saver import (
     VideoEncoders,
     InputPixelFormats,
     OutputPixelFormats,
     EncoderSpeedPresets,
+    check_gpu_availability,
+    check_ffmpeg_availability,
 )
-from .camera import CameraBackends
+from .camera import CameraInterfaces, CameraInformation, get_opencv_ids, get_harvesters_ids
 from .video_system import VideoSystem, extract_logged_video_system_data
 
 __all__ = [
-    "CameraBackends",
+    "CameraInformation",
+    "CameraInterfaces",
     "EncoderSpeedPresets",
     "InputPixelFormats",
     "OutputPixelFormats",
     "VideoEncoders",
     "VideoSystem",
+    "check_ffmpeg_availability",
+    "check_gpu_availability",
     "extract_logged_video_system_data",
+    "get_harvesters_ids",
+    "get_opencv_ids",
 ]
